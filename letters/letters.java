25c999275b5004dddc98bd6a7b1d1bc815dd8622
Bryan Glazer <bglazer@uark.edu>
2013-02-24 22:25:50 -0600

    Commit

--- letters/letters.java
+++ letters/letters.java
@@ -1,5 +1,13 @@
 import processing.core.*;
+import java.nio.file.Files;
+import java.io.File;
+import java.awt.image.DataBufferFloat;
+import java.awt.image.DataBuffer;
+import javax.imageio.ImageIO;
+import java.awt.image.WritableRaster; 
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.io.IOException;
 
 public class letters extends PApplet {
 
@@ -7,7 +15,8 @@ public class letters extends PApplet {
     float[][] noise_map;
 
     final float max_vel = 5;
-    final int num_particles = 1;
+    final int num_particles = 10;
+    float[][] map;
 
     public void setup()
     {
@@ -22,7 +31,7 @@ public class letters extends PApplet {
 
         for(int i = 0; i < num_particles; i++)
         {
-            PVector pos = new PVector(random(0,width), 0);
+            PVector pos = new PVector(random(0,width), random(0, height));
             PVector vel = new PVector(0, random(max_vel/2,max_vel));
             int color = colors[(int)random(0,3)];
             particles.add(new Particle(pos, vel, color, this)); 
@@ -40,13 +49,40 @@ public class letters extends PApplet {
                 point(i,j);
             }
         }
+
+        int[] text_map = new int[width*height];
+
+        text_map = extractBytes("a.png");
+
+        map = new float[width][height];
+
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                map[i][j] = noise_map[i][j] + (float)(text_map[j + i * height]/255f);
+            }
+        }
+    }
+
+    public int[] extractBytes (String ImageName) 
+    {
+        PImage p = loadImage(ImageName);
+        
+        image(p, 0, 0);   
+
+        loadPixels();
+
+        return pixels;
     }
 
     public void draw()
     {
+//        background(127,127,127, 0);
+
         for(Particle p : particles)
         {
-            p.steer(noise_map[(int)p.pos.x][(int)p.pos.y]);
+            p.steer(map[(int)p.pos.x][(int)p.pos.y]);
             p.move();
             p.render();
         }
